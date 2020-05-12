eaebd0236bf1cf35df28a44b624f9c7245a9e627
Nikita <lnv161297@gmail.com>
2020-05-12 21:09:39 +0300

    init project

---
+++ index.js
@@ -0,0 +1,55 @@
+const rp = require('request-promise')
+const cheerio = require('cheerio')
+const express = require('express')
+
+const app = express()
+
+app.set("view engine", "ejs");
+
+const REQUEST_URL = 'https://book24.ua'
+
+app.get('*', async function (req, res) {
+  res.render("app", {
+    title: "Web Scrapping Application",
+    books: await start()
+  });
+})
+
+app.listen(3000, function () {
+  console.log('happy web scrapping!')
+})
+
+async function start() {
+  const result = await rp(REQUEST_URL)
+  const $ = cheerio.load(result)
+
+  const books = $('.catalog-item-card')
+
+  const parsed = books.map(function () {
+    const src = $(this).find('.item_img').attr('src')
+    const imageSrc = `${REQUEST_URL}${src}`
+
+    const title = $(this).find('.item-title').text().trim()
+    const author = $(this).find('.article > a').text().trim()
+
+    const rating = $(this).find('.rating td').not('.vote-result').map(function () {
+      return !!$(this).find('div').hasClass('star-voted')
+    }).get().reduce(function (acc, item) {
+      return item ? ++acc : acc
+    }, 0)
+
+    const price = $(this).find('.catalog-item-price').text().trim()
+
+    return {
+      imageSrc,
+      title,
+      author,
+      rating,
+      price: price.replace(/\n|\t|[а-яА-я]/g, '').replace(/' '/g, '')
+    }
+  }).get();
+
+  return parsed
+}
+
+
